ee29d1f244ee285e5f6acc626371224387824181
ppuuffyy <ppuuffyy@gmail.com>
2020-04-10 19:49:39 +0300

    try to resolve toggle hidden

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyBtKoX3Sx_GPA_X3PCUavXoZasKNRrfmD8",
+  authDomain: "crwn-db-e66f1.firebaseapp.com",
+  databaseURL: "https://crwn-db-e66f1.firebaseio.com",
+  projectId: "crwn-db-e66f1",
+  storageBucket: "crwn-db-e66f1.appspot.com",
+  messagingSenderId: "492066227811",
+  appId: "1:492066227811:web:6689059146634d6e50da56",
+  measurementId: "G-3EPX0K80VM"
 };
 
 firebase.initializeApp(config);
